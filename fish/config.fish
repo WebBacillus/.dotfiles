ac55c2684d6c58cfd3157e9ad532f645cc5de95b
WebBacillus <webbalaka@gmail.com>
2025-12-18 12:17:59 +0700

    fix: resolve fish path conflict

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,15 @@ fzf --fish | source
 
 set -gx EDITOR nvim
 set -gx VISUAL nvim
+
+# Check the Operating System
+set -l os_name (uname)
+
+if test "$os_name" = "Darwin"
+    # --- MacOS Specific Paths ---
+    fish_add_path /Users/t.pasit.khumsena/.antigravity/antigravity/bin
+
+else if test "$os_name" = "Linux"
+    # --- Windows/WSL/Linux Specific Paths ---
+end
+
